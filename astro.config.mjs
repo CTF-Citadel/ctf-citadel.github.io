8682977bfc418bd458910d2016a5d2fb3021b308
0xk1f0 <fteppan@protonmail.com>
2023-09-22 09:29:06 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'ctf-citadel.github.io',
+	base: '/docs'
+	integrations: [
+		starlight({
+			title: 'CTF-Citadel',
+			social: {
+				github: 'https://github.com/CTF-Citadel',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
